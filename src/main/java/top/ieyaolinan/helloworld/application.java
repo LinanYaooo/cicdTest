682ef7effbf329835a107dcdce4ef48550ed2347
linany <ieyaolinan@gmail.com>
2021-01-02 12:07:01 +0800

    init

---
+++ src/main/java/top/ieyaolinan/helloworld/application.java
@@ -0,0 +1,11 @@
+package top.ieyaolinan.helloworld;
+
+public class application {
+    public static void main(String[] args) {
+        print("恭喜发财");
+    }
+
+    public static void print(Object obj){
+        System.out.println(obj.toString());
+    }
+}
